75aa39a7eb8d8f8a94d55e1726af5e55c1d6d4f2
Kene <kayyungx@gmail.com>
2022-10-20 20:06:08 +0100

    Change like functionality

--- src/modules/likes.js
+++ src/modules/likes.js
@@ -7,14 +7,15 @@ class Likes {
     this._parentElement.addEventListener("click", function (e) {
       const likebtn = e.target.closest(".recipe_icon");
       const likeNum = likebtn.nextElementSibling.firstElementChild;
+      const num = parseFloat(likeNum.textContent);
       let i = 0;
       if (!likebtn) return;
       if (likebtn.firstElementChild.classList.contains("heart-o")) {
         likebtn.innerHTML = `<use href="${icon}#icon-heart"></use>`;
-        likeNum.innerHTML = ++i;
+        likeNum.textContent = num + 1;
       } else {
         likebtn.innerHTML = `<use href="${icon}#icon-heart-o" class="heart-o"></use>`;
-        likeNum.innerHTML = 0;
+        likeNum.textContent = num - 1;
       }
     });
   }
