1e4e101514660b4e13c4f89842cda8e776e18e44
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-10-21 18:36:34 +0430

    Update counter.test.js

--- counter.test.js
+++ counter.test.js
@@ -1,6 +1,4 @@
 import { loadResult } from './src/modules/model.js';
-// eslint-disable-next-line spaced-comment
-/*eslint linebreak-style: ["error", "windows"]*/
 
 describe('Test for Items Counter', () => {
   test('Test Items counter', () => {
