956224e54f03fd5da0831348f7c26f19d287e558
Faranosh Amini <101713819+FaranoshAmini@users.noreply.github.com>
2022-10-21 14:43:13 +0430

    setup webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,36 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const HtmlWebpackInlineSVGPlugin = require("html-webpack-inline-svg-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: { bundle: path.resolve(__dirname, "src/modules/index.js") },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name][contenthash].js",
-    clean: true,
-    assetModuleFilename: "[name][ext]",
-  },
-  devtool: "source-map",
+  mode: 'production',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
   devServer: {
-    static: {
-      directory: path.resolve(__dirname, "dist"),
-    },
-    port: 7000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/Javascript-Captone-Project/',
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.(png|jpg|svg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack App",
-      filename: "index.html",
-      template: "src/template.html",
-    }),
-    new HtmlWebpackInlineSVGPlugin(),
-  ],
+  performance: {
+    hints: false,
+  },
 };
